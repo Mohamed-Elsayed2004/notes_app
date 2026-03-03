34858aa52db5c1804ba5a72dbbf46e664bc203c0
Mohamed-Elsayed2004 <mohamedelsayed20045@gmail.com>
2026-03-03 16:59:06 +0200

    create notes list view

--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:note_app/widgets/custom_app_bar.dart';
 import 'package:note_app/widgets/note_item.dart';
 import 'package:note_app/widgets/notes_list_view.dart';
+import 'package:note_app/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
@@ -9,13 +10,10 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 30),
-        child: Column(children: [CustomAppBar(), NotesListView()]),
-      ),
+      body: NotesViewBody(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: Icon(Icons.add),
+        child: Icon(Icons.add,color: Colors.black,),
         backgroundColor: Colors.cyanAccent,
       ),
     );
